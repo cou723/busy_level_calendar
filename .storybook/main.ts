94039a8449b6904140c98fd5963085d3f80af983
cou <courange.c@gmail.com>
2024-01-16 22:55:47 +0900

    add storybook setting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,41 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+const path = require('path');
 
-const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
+const react = require('@vitejs/plugin-react');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+
   framework: {
-    name: "@storybook/nextjs",
+    name: '@storybook/react-vite',
     options: {},
   },
+
+  async viteFinal(config) {
+    config.plugins = config.plugins.filter(
+      (plugin) => !(Array.isArray(plugin) && plugin[0]?.name.includes('vite:react'))
+    );
+
+    config.plugins.push(
+      react({
+        exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
+        },
+      })
+    );
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    };
+    return config;
+
+    console.log(config.plugins);
+    return config;
+  },
+
   docs: {
-    autodocs: "tag",
+    autodocs: true,
   },
 };
-export default config;
