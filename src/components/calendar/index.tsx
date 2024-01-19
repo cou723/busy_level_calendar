524f13774b82485bd6476bdd8379daacdddf8f01
cou <courange.c@gmail.com>
2024-01-20 03:46:39 +0900

    曜日の追加

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -6,11 +6,30 @@ import Header from './header';
 
 import type { YearMonth } from '@/types/yearMonth';
 
+import FlexBox from '@/components/utils/flexBox';
 import Neu from '@/components/utils/neu';
 import { getCalendarDates } from '@/libs/getCalendarDates';
 import { generateBusyLevels } from '@/types/busyLevel';
 import { type Calendar } from '@/types/calendar';
 
+const DayOfWeek: React.FC = () => {
+  const style = css`
+    flex: 1;
+    text-align: center;
+    font-weight: bold;
+  `;
+  return (
+    <FlexBox flexDirection="row">
+      <p css={[style, css({ color: '#ff5b5b' })]}>日</p>
+      <p css={style}>月</p>
+      <p css={style}>火</p>
+      <p css={style}>水</p>
+      <p css={style}>木</p>
+      <p css={style}>金</p>
+      <p css={[style, css({ color: '#2188ff' })]}>土</p>
+    </FlexBox>
+  );
+};
 interface CalendarProps {
   yearMonth: YearMonth;
   calendar: Calendar;
@@ -26,11 +45,12 @@ const Calendar: React.FC<CalendarProps> = ({ yearMonth, calendar, onPre, onNext,
         padding: 1rem;
         display: flex;
         flex-direction: column;
-        gap: 1rem;
+        gap: 0.3rem;
       `}
       {...props}
     >
       <Header {...{ yearMonth, onNext, onPre }} />
+      <DayOfWeek />
       <Grids
         dates={getCalendarDates(yearMonth)}
         schedules={calendar.schedules}
